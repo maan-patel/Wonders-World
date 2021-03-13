0ce2d943dfe94520325442f970fe913e9fc85dbb
maanpatel1 <65319014+maanpatel1@users.noreply.github.com>
2021-03-12 23:42:48 -0500

    updated content working

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 const wonders = require('./wonders')
@@ -18,43 +16,41 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
-const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20) + 10;
-        const camp = new Campground({
-            //YOUR USER ID
-            author: '603857fce5b64309fc07fa1e',
-            location: `${wonders[i].location}`,
-            title: `${wonders[i].name}`,
-            description: `${wonders[i].about}`,
-            price,
-            geometry: {
-                type: "Point",
-                coordinates: [
-                    wonders[i].longitude,
-                    wonders[i].latitude,
-                ]
-            },
-            images: [
-                {
-                    url: `${wonders[i].image}`,
-                    // filename: 'YelpCamp/ncjxioatxg93hp0jlnka'
-                },
+// const seedDB = async () => {
+//     await Campground.deleteMany({});
+//     for (let i = 0; i < 50; i++) {
+//         const price = Math.floor(Math.random() * 20) + 10;
+//         const camp = new Campground({
+//             //YOUR USER ID
+//             author: '603857fce5b64309fc07fa1e',
+//             location: `${wonders[i].location}`,
+//             title: `${wonders[i].name}`,
+//             description: `${wonders[i].about}`,
+//             price,
+//             geometry: {
+//                 type: "Point",
+//                 coordinates: [
+//                     wonders[i].longitude,
+//                     wonders[i].latitude,
+//                 ]
+//             },
+//             images: [
+//                 {
+//                     url: `${wonders[i].image}`,
+//                     // filename: 'YelpCamp/ncjxioatxg93hp0jlnka'
+//                 },
 
-            ]
-        })
-        await camp.save();
-    }
-}
+//             ]
+//         })
+//         await camp.save();
+//     }
+// }
 
-seedDB()
-    .then(() => {
-        mongoose.connection.close();
-    })
+// seedDB()
+//     .then(() => {
+//         mongoose.connection.close();
+//     })
 
-module.exports = { seedDB }
+// module.exports = { seedDB }

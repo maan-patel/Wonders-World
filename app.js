dcf20fb276f3a7412ea05e4c63f4d2bb05ee8ecb
Maan Patel <79115388+maan-patel@users.noreply.github.com>
2021-07-25 14:14:32 -0400

    Updated View Pages for Mobile Friendly Views

--- app.js
+++ app.js
@@ -91,7 +91,6 @@ const scriptSrcUrls = [
     "https://kit.fontawesome.com/",
     "https://cdnjs.cloudflare.com/",
     "https://cdn.jsdelivr.net",
-    "https://wonders-world.herokuapp.com/",
 ];
 const styleSrcUrls = [
     "https://kit-free.fontawesome.com/",
@@ -100,14 +99,12 @@ const styleSrcUrls = [
     "https://api.tiles.mapbox.com/",
     "https://fonts.googleapis.com/",
     "https://use.fontawesome.com/",
-    "https://wonders-world.herokuapp.com/",
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
     "https://a.tiles.mapbox.com/",
     "https://b.tiles.mapbox.com/",
     "https://events.mapbox.com/",
-    "https://wonders-world.herokuapp.com/",
 ];
 const fontSrcUrls = [];
 app.use(
@@ -134,7 +131,6 @@ app.use(
                 "https://images.theconversation.com/",
                 "https://lp-cms-production.imgix.net/",
                 "https://www.wallpapertip.com/wmimgs/",
-                "https://wonders-world.herokuapp.com/"
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
         },
